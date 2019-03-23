0963c6957fd2df34e3aa44e412e4c2d7a47849bb
Felipe Ponto <felipemsaieg@gmail.com>
2019-03-23 11:36:38 -0300

    movimentos do rei

--- src/xadrez/peças/Rei.java
+++ src/xadrez/peças/Rei.java
@@ -1,5 +1,6 @@
 package xadrez.peças;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
 import xadrez.Cor;
 import xadrez.PeçaXadrez;
@@ -18,6 +19,17 @@ public class Rei extends PeçaXadrez {
 	@Override
 	public boolean[][] movimentosPossiveis() {
 		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+		Posicao p = new Posicao(0, 0);
+
+		for (int i = -1; i <= 1; i++) {
+			for (int j = -1; j <= 1; j++) {
+				p.setPosicao(posicao.getLinha() - i, posicao.getColuna() - j);
+				if (getTabuleiro().posicaoExiste(p) && (!getTabuleiro().temPeça(p) || temPeçaOponente(p))) {
+					mat[p.getLinha()][p.getColuna()] = true;
+				}
+			}
+		}
+
 		return mat;
 	}
 }
